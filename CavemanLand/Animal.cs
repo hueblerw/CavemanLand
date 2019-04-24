20aee7073cb34b17f82ff5f95e2015df968fc9a3
williamhuebler <william.huebler@trunkclub.com>
2019-04-23 20:57:03 -0500

    initial commit and creation of the animal and plant classes

---
+++ CavemanLand/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace CavemanLand
+{
+    public class Animal
+    {
+		private string name;
+		private bool formsHerds;
+		private List<string> habitats;
+		private List<int> abundance;
+		private List<string> foodType;
+		private double foodEaten;
+		private int defense;
+		private int attack;
+		private int[] temperatureTolerance = new int[2];
+		private double foodPerAnimal;
+		private double weightPerUnit;
+		private List<string> production;
+		private List<double> productionPerUnit;
+		private bool isRidable;
+		private bool isBurden;
+
+        public Animal()
+        {
+        }
+    }
+}
---
+++ CavemanLand/Plant.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CavemanLand
+{
+    public class Plant
+    {
+		private string name;
+		private string type;
+		private bool isFarmable;
+		private List<string> habitats;
+		private List<int> abundance;
+		private int[] temperatureRange = new int[2];
+		private double[] rainfallRange = new double[2];
+		private int growthPeriod;
+		private bool doesFrostKill;
+		private string resourceProduced;
+		private double unitPerHarvest;
+		private double weightPerUnit;
+
+        public Plant()
+        {
+        }
+    }
+}
