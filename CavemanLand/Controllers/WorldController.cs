10946974b82e883b089fcdc913ee301659285da4
williamhuebler <william.huebler@trunkclub.com>
2019-04-24 08:08:27 -0500

    add a world controller

---
+++ CavemanLand/Controllers/WorldController.cs
@@ -0,0 +1,13 @@
+﻿namespace CavemanLand.Controllers
+{
+    public class WorldController
+    {
+		static void Main(string[] args)
+		{
+			// Load all animals
+			Animal[] animals = new Animal[15];
+            // Load all Plants
+			Plant[] plants = new Plant[10];
+		}
+    }
+}
