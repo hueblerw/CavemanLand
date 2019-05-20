87c9501231979a4511f976abd5781941e4d4462e
williamhuebler <william.huebler@trunkclub.com>
2019-05-20 16:55:52 -0500

    temperature equation tests are in, but the equation itself does not work

---
+++ CavemanLand/Generators/TemperatureEquation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CavemanLand.Generators
+{
+    public class TemperatureEquation
+    {
+        private double A;
+        private double B;
+        private double C;
+
+        private double curveMin;
+        private double variance;
+        private double amplitude;
+        private Random randy;
+
+        public TemperatureEquation(int lowTemp, int highTemp, int summerLength, double variance)
+        {
+            A = (summerLength - 1) / ((-1.0 / 4.0) * summerLength * (Math.Pow(summerLength, 2) - 4.0));
+            B = -3.0 * A;
+            C = 1.0 - A - B;
+            this.variance = variance;
+            amplitude = (highTemp - lowTemp) / 2.0;
+            curveMin = lowTemp + amplitude;
+            randy = new Random();
+        }
+
+        public int getTodaysTemp(int day)
+        {
+            double x = (day - 1) * (Math.PI / 60.0);  // Do something to the day.  Should go from 0 to 2 PI, but days 1 to 120
+            double G = A * Math.Pow(x, 3) + B * Math.Pow(x, 2) + C * x;
+            double curveTemp = -amplitude * Math.Cos(Math.PI * G) + curveMin;
+            double temp = variance * randy.NextDouble() - (variance / 2.0) + curveTemp;
+            return (int)Math.Round(temp, 0);
+        }
+
+        public override string ToString()
+        {
+            return "-" + amplitude + "cos(PI * [" + A + "x^3 + " + B + "x^2 + " + C + "x]) + " + curveMin;
+        }
+    }
+}
---
+++ CavemanLand/UnitTests/TemperatureEquationTestClass.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using CavemanLand.Generators;
+using System;
+
+namespace CavemanLand.UnitTests
+{
+    [TestFixture()]
+    public class TemperatureEquationTestClass
+    {
+        [Test()]
+		public void TemperatureInGeneralRangeTestCase()
+        {
+			TemperatureEquation temperatureEquation = new TemperatureEquation(20, 80, 60, 6.0);
+			Console.WriteLine(temperatureEquation.ToString());
+			for (int day = 1; day <= 120; day++){
+				int temp = temperatureEquation.getTodaysTemp(day);
+				Console.Write(temp + ", ");
+				assertBetween(temp, 20 - 6 / 2, 80 + 6 / 2);            
+			}
+        }
+        
+		[Test()]
+        public void BoundariesTemperaturesCorrectTestCase()
+        {
+            TemperatureEquation temperatureEquation = new TemperatureEquation(20, 80, 60, 6.0);         
+            assertBetween(temperatureEquation.getTodaysTemp(1), 20 - 6 / 2, 20 + 6 / 2);
+            assertBetween(temperatureEquation.getTodaysTemp(120), 20 - 6, 20 + 6 / 2);
+            assertBetween(temperatureEquation.getTodaysTemp(61), 80 - 6 / 2, 80 + 6 / 2);
+			assertBetween(temperatureEquation.getTodaysTemp(91), 50 - 6 / 2, 50 + 6 / 2);
+            assertBetween(temperatureEquation.getTodaysTemp(31), 50 - 6 / 2, 50 + 6 / 2);
+        }
+
+        private void assertBetween(int value, int lowerBound, int higherBound)
+		{
+			Assert.LessOrEqual(value, higherBound);
+            Assert.GreaterOrEqual(value, lowerBound);
+		}
+    }
+}
